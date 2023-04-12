b5b778720a817bb6c91dfb200a89519acd566a49
helboi4 <h.hopecarter@gmail.com>
2023-04-12 23:14:14 +0100

    Very basic audio visualiser

---
+++ script.js
@@ -0,0 +1,49 @@
+
+const container = document.getElementById("container");
+const canvas = document.getElementById("canvas1");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext("2d");
+let audioSource;
+let analyser;
+
+container.addEventListener("click", function(){
+    const audio1 = document.getElementById("audio1");
+    audio1.src = "./audio_assets/audio1.wav";
+    const audioCtx = new (window.AudioContext || webkitURL.webkitAudio)();
+
+    audio1.play();
+    audioSource = audioCtx.createMediaElementSource(audio1);
+    analyser = audioCtx.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioCtx.destination);
+    analyser.fftSize = 64;
+    //number of bars in our visualiser (half of fftSize)
+    const bufferLength = analyser.frequencyBinCount;
+    //array for unassigned 8 bit integers
+    const dataArray = new Uint8Array(bufferLength);
+
+    //making the width of the bars add up to fit screen
+    const barWidth = canvas.width/bufferLength;
+    let x;
+
+    function animate(){
+        x=0;
+        //clear whole canvas
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        //gets frequency data (the volume of each frequency chunk) to use as height of bars
+        analyser.getByteFrequencyData(dataArray);
+        for(let i = 0; i < bufferLength; i++){
+            //setting height to volume of frequency
+            barHeight = dataArray[i];
+            ctx.fillStyle = "white";
+            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+            x+=barWidth;
+        }
+        //call parent function to create animation loop
+        requestAnimationFrame(animate);
+    }
+    animate();
+});
+
+
