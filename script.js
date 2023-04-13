1356e0998739fbd9dcc7caf7000167660b4965a0
helboi4 <h.hopecarter@gmail.com>
2023-04-13 16:10:54 +0100

    Allow file upload

--- script.js
+++ script.js
@@ -11,7 +11,39 @@ let analyser;
 //TODO: fix issue with file upload button not working
 
 file.addEventListener("change", function(){
-    console.log(this.files);
+    const files = this.files;
+    const audio1 = document.getElementById("audio1");
+    audio1.src = URL.createObjectURL(files[0]);
+    const audioCtx = new (window.AudioContext || webkitURL.webkitAudio)();
+    audio1.load();
+    audio1.play();
+    
+    audioSource = audioCtx.createMediaElementSource(audio1);
+    analyser = audioCtx.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioCtx.destination);
+    analyser.fftSize = 64;
+    //number of bars in our visualiser (half of fftSize)
+    const bufferLength = analyser.frequencyBinCount;
+    //array for unassigned 8 bit integers
+    const dataArray = new Uint8Array(bufferLength);
+
+    //making the width of the bars add up to fit screen
+    const barWidth = canvas.width/bufferLength;
+    let barHeight;
+    let x;
+
+    function animate(){
+        x=0;
+        //clear whole canvas
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        //gets frequency data (the volume of each frequency chunk) to use as height of bars
+        analyser.getByteFrequencyData(dataArray);
+        drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray)
+        //call parent function to create animation loop
+        requestAnimationFrame(animate);
+    }
+    animate();
 })
 
 // container.addEventListener("click", function(){
@@ -40,16 +72,19 @@ file.addEventListener("change", function(){
 //         ctx.clearRect(0,0, canvas.width, canvas.height);
 //         //gets frequency data (the volume of each frequency chunk) to use as height of bars
 //         analyser.getByteFrequencyData(dataArray);
-//         for(let i = 0; i < bufferLength; i++){
-//             //setting height to volume of frequency
-//             barHeight = dataArray[i];
-//             ctx.fillStyle = "white";
-//             ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-//             x+=barWidth;
-//         }
+//         drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray)
 //         //call parent function to create animation loop
 //         requestAnimationFrame(animate);
 //     }
 //     animate();
 // });
 
+function drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray){
+    for(let i = 0; i < bufferLength; i++){
+        //setting height to volume of frequency
+        barHeight = dataArray[i];
+        ctx.fillStyle = "white";
+        ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+        x+=barWidth;
+    }
+}
